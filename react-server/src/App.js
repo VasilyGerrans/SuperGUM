35ffcb5feddf7a480e0c6ea650bbe158dce37793
Vasily Gerrans <vasilygerrans@macbook-pro.home>
2021-09-22 07:41:11 +0100

    Basic web3 wallet detection functionality.

--- react-server/src/App.js
+++ react-server/src/App.js
@@ -2,22 +2,32 @@ import StickyHeader from './StickyHeader';
 import CreatorContent from './CreatorContent';
 import SubscriptionContent from './SubscriptionContent';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useMoralis } from 'react-moralis';
 
 function App () {
-  const { isAuthenticated, isWeb3Enabled } = useMoralis();
+  const { isAuthenticated, isWeb3Enabled, enableWeb3, user } = useMoralis();
 
   const [ contentUnlocked, setContentUnlocked ] = useState(true);
+
+  useEffect(() => {
+    if (isAuthenticated === true && isWeb3Enabled === false) {
+      enableWeb3();
+    }
+  }, [isAuthenticated]);
 
   const walletButton = () => {
     console.log(isAuthenticated, isWeb3Enabled);
     setContentUnlocked(!contentUnlocked);
+    if (isWeb3Enabled === false) {
+      enableWeb3();
+      console.log(user.attributes.ethAddress);
+    }
   }
 
   return (
     <div className="App">
-      <StickyHeader walletButton={walletButton} />
+      <StickyHeader walletButton={walletButton} isWeb3Enabled={isWeb3Enabled} user={user} />
       <CreatorContent />
       <SubscriptionContent unlocked={contentUnlocked} />
     </div>
--- react-server/src/StickyHeader.js
+++ react-server/src/StickyHeader.js
@@ -14,9 +14,15 @@ function StickyHeader(props) {
                     </h2>
                 </Navbar.Brand>
                 <Navbar.Collapse className="justify-content-end">
-                    <Button onClick={() => {props.walletButton()}}>
-                        Connect wallet
-                    </Button>
+                    {props.isWeb3Enabled ? 
+                        <Button>
+                            {props.user.attributes.ethAddress.toString().substring(0, 4)}...{props.user.attributes.ethAddress.toString().substring(38)}
+                        </Button>
+                    :
+                        <Button onClick={() => {props.walletButton()}}>
+                            Connect wallet
+                        </Button>
+                    }
                 </Navbar.Collapse>
             </Navbar>
         </div>
