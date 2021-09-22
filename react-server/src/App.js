22bf090b963970800a40330fb94d9cef0b9f8e10
Vasily Gerrans <vasilygerrans@macbook-pro.home>
2021-09-22 08:20:16 +0100

    Clearing up App.js and App.css

--- react-server/src/App.js
+++ react-server/src/App.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 import { useMoralis } from 'react-moralis';
 
 function App () {
-  const { isAuthenticated, isWeb3Enabled, enableWeb3, user } = useMoralis();
+  const { isAuthenticated, isWeb3Enabled, enableWeb3, user, Moralis } = useMoralis();
 
   const [ contentUnlocked, setContentUnlocked ] = useState(true);
 
@@ -16,12 +16,26 @@ function App () {
     }
   }, [isAuthenticated]);
 
+  useEffect(() => {
+    /* 
+    Can be used later for dynamically changing the dapp appearance
+    when the user connects their wallet. We're not including functionality
+    for disconnecting. If the user decides to manually disconnect MetaMask, 
+    that is not considered a normal way to interract with the dapp. The dapp
+    would have to have authentication protections to ensure people can't
+    arbitrarily disconnect MetaMask and then do something in an account belonging
+    to that MetaMask account. (So, there will be checks on interacting with user
+    details).
+
+    We can later add a "Disconnect?" window when a user clicks on their wallet
+    address button, but that is more of a "brushing up" feature.
+    */
+    console.log("web3 state changed");
+  }, [isWeb3Enabled])
+
   const walletButton = () => {
-    console.log(isAuthenticated, isWeb3Enabled);
-    setContentUnlocked(!contentUnlocked);
     if (isWeb3Enabled === false) {
       enableWeb3();
-      console.log(user.attributes.ethAddress);
     }
   }
 
