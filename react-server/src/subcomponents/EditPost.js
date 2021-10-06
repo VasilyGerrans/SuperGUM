34e7f7e5661d92d7fb911d8939ac58bf2c4c4f95
Vasily Gerrans <->
2021-10-06 13:20:28 +0100

    Some minor styling

--- react-server/src/subcomponents/EditPost.js
+++ react-server/src/subcomponents/EditPost.js
@@ -4,6 +4,16 @@ import { Card, Button } from 'antd';
 function EditPost(props) {
     const [ text, setText ] = useState("");
     const [ warningMsg, setWarningMsg ] = useState("");
+    const [ selectedFile, setSelectedFile ] = useState(null);
+
+    const onFileChange = e => {
+        if (e.target.files.length > 0) {
+            setSelectedFile(e.target.files[0]);
+        }
+        else {
+            setSelectedFile(null);
+        }
+    }
 
     return (
         <Card className="SubscriptionContent" style={{
@@ -48,14 +58,50 @@ function EditPost(props) {
                     }
                 </div>
                 <div>
+                    <input type="file" id="selectedFile" style={{display: "none"}} 
+                        onChange={onFileChange}
+                    />
+                        {selectedFile !== null && selectedFile.name ?
+                        <span>
+                            {selectedFile.name}
+                        </span>    
+                        :
+                        <span>
+                            No file chosen.
+                        </span>    
+                        } 
+                    &nbsp;
+                    <input 
+                        type="button" 
+                        value="Select file"
+                        className="dapp-button"
+                        onClick={() => {
+                            document.getElementById("selectedFile").click()
+                        }} 
+                    />
                     <Button className="cancel-button" onClick={() => {
                         props.setContentCreationMode(false);
                     }}>
                         Cancel
                     </Button>
                     <Button className="confirm-button" onClick={() => {
-                        if (text.trim() === "" && warningMsg === "") {
-                            setWarningMsg("Error: please enter some content before publishing.");
+                        if (text.trim() === "") {
+                            if (selectedFile === null) {
+                                setWarningMsg("Error: please enter some content before publishing.");
+                            }
+                            else {
+                                if (["image/png", "image/jpeg", "image/gif", "video/mp4", "audio/mpeg"].includes(selectedFile.type)) {
+                                    if (selectedFile.size < 5242880) {
+                                        console.log("READY TO SEND");
+                                    }
+                                    else {
+                                        setWarningMsg("Error: only files under 5MB allowed.");
+                                    }
+                                }
+                                else {
+                                    setWarningMsg("Error: the only supported formats are png, jpeg, gif, mp3 and mp4.");
+                                }
+                            }
                         }
                         else {
                             props.createContent(text.trim());
